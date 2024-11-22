0819c7cc1097e71bc235f47d5658311646eb8895
JuanCuelloVN <juan.cuello@viewnext.com>
2024-11-22 09:20:03 +0100

    Commit para la rama develop

--- src/ejemploEclipse/Main.java
+++ src/ejemploEclipse/Main.java
@@ -3,7 +3,8 @@ package ejemploEclipse;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Hola" );
+		System.out.println("Este es un comentario escrito para la rama develop");
 	}
 
 }
