1f46fe5473ce081765e4446e5e621b393ed7c289
JuanCuelloVN <juan.cuello@viewnext.com>
2024-11-22 09:22:46 +0100

    comentarios

--- src/ejemploEclipse/Main.java
+++ src/ejemploEclipse/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola" );
 		System.out.println("Este es un comentario escrito para la rama develop");
+		//Estos comentarios tienen falstats de ortrografiA
+		//Estoi en viunes pero nose escribi, solo pogramar
 	}
 
 }
